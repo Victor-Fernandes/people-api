0047421593d348ec3cf6f0de90c42c87961ee5b7
victor <victor.fernandes.07@gmail.com>
2020-11-27 01:08:30 -0300

    add propierties

--- src/main/java/one/digitalinovation/personapi/PersonapiApplication.java
+++ src/main/java/one/digitalinovation/personapi/PersonapiApplication.java
@@ -1,6 +1,5 @@
 package one.digitalinovation.personapi;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
